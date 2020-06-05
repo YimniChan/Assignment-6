de8bb7282946305d4eb5c0287275f597d4decf9b
viviantom0506 <icemarina123@gmail.com>
2020-06-04 20:40:12 -0400

    added the final code for Zipsearch.js

---
+++ src/Component/Zipsearch.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+//import './Zip.css';
+import axios from "axios";
+class Zipsearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: [],
+      zipValue: "",
+    };
+    this.handleZipSearch = this.handleZipSearch.bind(this);
+  }
+  handleZipSearch(e) {
+    this.setState({ zipValue: e.target.value });  
+    const url = "http://ctp-zip-api.herokuapp.com/zip/" +  e.target.value;
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data;
+        this.setState({ zipCode: data });
+      })
+      .catch((err) =>{ console.log(err)
+        this.setState({zipCode:[]})
+    });
+  }
+  render() {
+    let display;
+    if (this.state.zipCode.length === 0) {
+      display = <div>Not Found</div>;
+    } else {
+      display = this.state.zipCode.map((data, i) => (
+        <div class="zipdetail" key={i}>
+          {data.LocationText}
+          <ul>
+            <li>State: {data.State}</li>
+            <li>
+              Location: ({data.Lat}, {data.Long})
+            </li>
+            <li>Population (estimated):{data.EstimatedPopulation}</li>
+            <li>Total Wages:{data.TotalWages}</li>
+          </ul>
+        </div>
+      ));
+    }
+    return (
+      <div>
+        <h1>Zip Code Search</h1>
+        <div className="search">
+          <label htmlFor="zipCode">Enter a zip code: </label>
+          <input
+            type="text"
+            pattern="[0-9]*"
+            maxlength="5"
+            name="zip"
+            placeholder={this.state.zipValue}
+            onChange={this.handleZipSearch}>
+            </input>
+          <br />
+          <br />
+        </div>
+        <div class="zipdata">{display}</div>
+      </div>
+    );
+  }
+}
+export default Zipsearch;
