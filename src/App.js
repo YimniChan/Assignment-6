1c7e8114d5e1b4ec94be9d8ed8ee14c72459cd45
viviantom0506 <icemarina123@gmail.com>
2020-06-04 20:39:44 -0400

    added the final code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import Zip from './components/Zip.css';
+import Zipsearch  from './Component/Zipsearch.js'
 import './App.css';
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <Zipsearch zipPlaceHolder=""/>
+    );
+  }
 }
-
 export default App;
